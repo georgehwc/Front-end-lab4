ee98c0389891db063a3f9612e950a1494ec21cfd
p17238710 <p17238710@my365.dmu.ac.uk>
2020-02-14 15:46:04 +0000

    lab4 work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+// const x;
+// const y;
+
+var x = document.getElementById(menu-toggle);
+
+var y = document.getElementById(menu-nav);
+
+console.log("called toggleMenu");
+
+document.getElementById("menu-toggle").addEventListener("click", display:flex);
+
+function toggleMenu{
+
+
+
+}
